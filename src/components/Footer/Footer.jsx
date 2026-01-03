61324c6fada2e4cc873022f78895cad348f7d1da
architpatle <architpatle06@gmail.com>
2026-01-03 15:06:46 +0530

    Footer Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,65 +1,95 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import { LuInstagram, LuFacebook, LuYoutube } from "react-icons/lu";
+import {
+  LuArrowUpRight,
+  LuPhone,
+  LuMail
+} from "react-icons/lu";
+import logo from "../../assets/images/Wanders-XO-Logo.png"
+import img1 from "../../assets/images/paris.jpg";
+import img2 from "../../assets/images/japan.jpg";
+import img3 from "../../assets/images/bali.jpg";
+import img4 from "../../assets/images/dubai.jpg";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.top}>
 
-        {/* Brand / Contact Form */}
-        <div className={styles.contact}>
-          <h4>Let’s Connect</h4>
-
-          <div className={styles.formGrid}>
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
-            <input type="email" placeholder="Email *" />
-            <input type="tel" placeholder="Phone" />
+        {/* BRAND */}
+        <div className={styles.brand}>
+          <div className={styles.logo}>
+            <img height={50} src={logo} alt="LOGO"/>
           </div>
-
-          <textarea placeholder="Type your message here..." />
-
-          <button className={styles.primaryBtn}>Submit</button>
-        </div>
-
-        {/* Newsletter */}
-        <div className={styles.newsletter}>
-          <h4>Weekly Travel Inspiration</h4>
+          <h3>Wander XO</h3>
           <p>
-            Get destination ideas, travel tips, and exclusive offers delivered
-            straight to your inbox.
+            Thoughtfully designed journeys, built for people who
+            value experience over itineraries.
           </p>
 
-          <input type="text" placeholder="Full Name" />
-          <input type="email" placeholder="Email *" />
-
-          <button className={styles.secondaryBtn}>Subscribe</button>
+          <div className={styles.socials}>
+            <span>FACEBOOK</span>
+            <span>INSTAGRAM</span>
+          </div>
         </div>
 
-        {/* Quick Links */}
+        {/* LINKS */}
         <div className={styles.links}>
-          <h4>Quick Links</h4>
           <ul>
-            <li>Cancellation Policy</li>
-            <li>Terms & Conditions</li>
-            <li>Privacy Policy</li>
+            <li><LuArrowUpRight /> Recent Journeys</li>
+            <li><LuArrowUpRight /> Travel Styles</li>
+            <li><LuArrowUpRight /> About Us</li>
+            <li><LuArrowUpRight /> Gallery</li>
+          </ul>
+
+          <ul>
+            <li><LuArrowUpRight /> Branding</li>
+            <li><LuArrowUpRight /> Web & Interactive</li>
+            <li><LuArrowUpRight /> Merchandise</li>
+            <li><LuArrowUpRight /> Experiences</li>
+          </ul>
+
+          <ul>
+            <li><LuArrowUpRight /> Our Story</li>
+            <li><LuArrowUpRight /> Help & Support</li>
+            <li><LuArrowUpRight /> Awards</li>
+            <li><LuArrowUpRight /> Contact</li>
           </ul>
         </div>
       </div>
 
-      {/* Bottom */}
+      {/* DIVIDER */}
+      <div className={styles.divider} />
+
+      {/* BOTTOM */}
       <div className={styles.bottom}>
-        <div className={styles.info}>
-          <p>Email: hi@wanderxo.com</p>
-          <p>Phone: +91-9503-889-337</p>
+        {/* GALLERY */}
+        <div className={styles.gallery}>
+          <img src={img1} alt="" />
+          <img src={img2} alt="" />
+          <img src={img3} alt="" />
+          <img src={img4} alt="" />
         </div>
 
-        <div className={styles.socials}>
-          <LuFacebook />
-          <LuInstagram />
-          <LuYoutube />
+        {/* NEWSLETTER */}
+        <div className={styles.newsletter}>
+          <h4>Newsletter</h4>
+          <div className={styles.subscribe}>
+            <input type="email" placeholder="Enter your email address" />
+            <button>Subscribe</button>
+          </div>
         </div>
+
+        {/* CONTACT */}
+        <div className={styles.contact}>
+          <p><LuPhone /> +91-9503-889-337</p>
+          <p><LuMail /> hi@wanderxo.com</p>
+        </div>
+      </div>
+
+      {/* COPYRIGHT */}
+      <div className={styles.copyright}>
+        © Copyright 2026. All Rights Reserved.
       </div>
     </footer>
   );
